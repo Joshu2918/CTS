43b2972e755d2cd35091fd32fa9f5f09465a5a27
Joshu2918 <peacock23499@gmail.com>
2026-08-13 08:48:39 +0530

    Complete Angular Hands-On 2

--- DeepSkilling/Module-08-Angular/HandsOn/Student_Course_Portal/src/app/app.routes.ts
+++ DeepSkilling/Module-08-Angular/HandsOn/Student_Course_Portal/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
+import { CoursesComponent } from './pages/courses/courses';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'courses',
+    component: CoursesComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
   }
 ];
--- DeepSkilling/Module-08-Angular/HandsOn/Student_Course_Portal/src/app/pages/home/home.ts
+++ DeepSkilling/Module-08-Angular/HandsOn/Student_Course_Portal/src/app/pages/home/home.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  OnDestroy,
+  SimpleChanges
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input() portalName = 'Student Course Portal';
+
+  studentName = 'Joshu';
+
+  courseCount = 12;
+  enrolledCount = 3;
+  gpa = 3.8;
+
+  isPortalActive = true;
+
+  message = '';
+
+  ngOnInit() {
+    console.log('Home component initialized');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('Input value changed:', changes);
+  }
+
+  enroll() {
+    this.message = 'Enrollment successful!';
+  }
+
+  ngOnDestroy() {
+    console.log('Home component destroyed');
+  }
 }
